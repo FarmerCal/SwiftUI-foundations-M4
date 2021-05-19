4da71bcd74f7c6999508d47cb3304c5b12d08abe
callum day ham <caldayham@gmail.com>
2021-05-18 19:03:45 -0700

    Initial Commit

---
+++ Recipe List App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipe List App
+//
+//  Created by Callum Day Ham on 5/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Callum Day Ham on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
